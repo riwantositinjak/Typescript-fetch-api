398ce31cc0c9f6e5ba9470eb5330d5534be0f068
riwantositinjak <riwanto1212@gmail.com>
2022-12-06 22:05:30 +0700

    test

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-// test('should first', () => { second })
+interface User {
+    name: string;
+    id: number;
+  }
+   
+  class UserAccount {
+    name: string;
+    id: number;
+   
+    constructor(name: string, id: number) {
+      this.name = name;
+      this.id = id;
+    }
+  }
+   
+  const user: User = new UserAccount("Murphy", 1);

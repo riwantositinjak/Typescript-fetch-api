af8d74e57b2eeb29152eb72e39850124a6802cc7
riwantositinjak <riwanto1212@gmail.com>
2022-12-07 14:49:31 +0700

    catching errors with typescript using interface

--- index.ts
+++ index.ts
@@ -2,11 +2,20 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 
+// interfaces in typescript are used to define the structure of an object 
+// inside of an interface we can freely ignore properties if we want to
+
+interface Todo {
+    id : number,
+    title : string,
+    completed : boolean,
+}
+
 axios.get(url).then(response => {
-    const Todo = response.data;
-    const ID = Todo.ID;
-    const title = Todo.Title;
-    const finished = Todo.finished;
+    const Todo = response.data as Todo ; // as Todo berguna untuk auto correct dan membantu developer dalam mengoreksi code
+    const ID = Todo.id;
+    const title = Todo.title;
+    const finished = Todo.completed;
 
     console.log(`
     The id is : ${ID}

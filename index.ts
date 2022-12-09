31445e633e2abe288060449c9d933a47f06aacb8
riwantositinjak <riwanto1212@gmail.com>
2022-12-09 20:15:26 +0700

    catching error typescript with adding semicolon type variable

--- index.ts
+++ index.ts
@@ -6,20 +6,26 @@ const url = "https://jsonplaceholder.typicode.com/todos/1"
 // inside of an interface we can freely ignore properties if we want to
 
 interface Todo {
-    id : number,
-    title : string,
-    completed : boolean,
+    id: number,
+    title: string,
+    completed: boolean,
 }
 
 axios.get(url).then(response => {
-    const Todo = response.data as Todo ; // as Todo berguna untuk auto correct dan membantu developer dalam mengoreksi code
+    const Todo = response.data as Todo; // as Todo berguna untuk auto correct dan membantu developer dalam mengoreksi code
     const ID = Todo.id;
     const title = Todo.title;
     const finished = Todo.completed;
 
-    console.log(`
-    The id is : ${ID}
+    console.log(logTodo(ID, finished, title))
+})
+
+const logTodo = (ID : number, title : string, finished : boolean) => {
+    console.log(` 
+    The id is : ${ID}z
     and The Title is : ${title}
     and is it completed : ${finished} 
     `)
-})
+}
+
+// disini urutan dalam argument dalam typescript sangat penting, maka dari itu tiap argumen perlu diberikan type datanya 
